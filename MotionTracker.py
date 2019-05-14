0201e3f6523c3d39cd3432c86b38d543f2c34a8b
Kelson Cantrell <cantrellkelson@gmail.com>
2019-05-13 20:14:17 -0700

    I did a thing

--- MotionTracker.py
+++ MotionTracker.py
@@ -34,18 +34,8 @@ class MotionTracker:
             self.direction = 'west'
         elif self.direction == 'west':
             self.direction = 'north'
-
-    def move(self):
-        if self.direction == 'north':
-            self.y += 1
-        elif self.direction == 'west':
-            self.x -= 1
-        elif self.direction == 'south':
-            self.y -= 1
-        elif self.direction == 'east':
-            self.x += 1
             
-    def selfmove(self, distance):
+    def move(self, distance = 1):
         if self.direction == 'north':
             self.y += distance
         elif self.direction == 'west':
@@ -56,38 +46,14 @@ class MotionTracker:
             self.x += distance
 #------------------------------------------(the real stuff)---------------------------------------------------
 
-def playgame():
-    play = Player()
-    while True:
-        
-        if play.mt.get_x() == 1 and play.mt.get_y() == 1:
-            play.take_damage(damage_taken)
-            play.stats(2)
-            
-        if play.mt.get_x() == -1 and play.mt.get_y() == -1:
-            play.poison2 = True
-            print('you stepped on a gu mine! Oh no too bad youll be taking damage of 2 each step owo')
-            
-        if play.health() == 0:
-            print('''---
-game over (ya suck)
----''')
-            break
 
-        command = input("What dyou wanna do?\n => ")
-        result = process_command(play.mt, command)
-        print(result)
-
-        
-#------------------------------------------(class Player [play])----------------------------------------------------
 
 class Player():
     def __init__(self):
         self.mt = MotionTracker()
         self.hp = 10
         self.healpot = 2
-        self.poison2 = False
-        self.action = False
+        self._poison = False
         
     def health(self):
         return self.hp
@@ -107,77 +73,91 @@ class Player():
 this is why you don't spam heal your level 6 bulbasaur smh
 ---
 ''')
-                
     def poison(self):
-        if poison2 == True:
-            if action == True:
-                self.hp -= 2
-                stats()
+        self._poison = True
 
-    def stats(damage_taken):
+    def status(self):
+        if self._poison:
+            self.take_damage(1)
+    
+    def stats(self, damage_taken):
         print('health:',  health())
         print('damage taken:', damage_taken)
 
-    def movement():
-        self.action = True
-        self.action = False
-
-#------------------------------------------(Process_command)----------------------------------------------
-
-def process_command(mt, command):
-    play = Player()
-    # go forward, turn left, turn right, location
-    if command == 'go forward':
-        mt.move()
-        play.movement()
-        return '''✔
+#------------------------------------------(Game)--------------------------------------------------------------
+class Game():
+    def __init__(self):
+        self.gu_mine = {(-1, -1)}
+    #------------------------------------------(process_command)-----------------------------------------------
+    def process_command(self, mt, command):
+        play = Player()
+        # go forward, turn left, turn right, location
+        if command == 'go forward':
+            mt.move()
+            return '''✔
         
 ---'''
-    if command == 'turn left':
-        mt.turn_left()
-        play.movement()
-        return '''✔
+        if command == 'turn left':
+            mt.turn_left()
+            return '''✔
 
 
 ---'''
-    if command == 'turn right':
-        mt.turn_right()
-        play.movement()
-        return '''✔
+        if command == 'turn right':
+            mt.turn_right()
+            return '''✔
 
 ---'''
-    if command == 'location':
-        return('({}, {})'.format(mt.get_x(), mt.get_y()))
+        if command == 'location':
+            return('({}, {})'.format(mt.get_x(), mt.get_y()))
 
-    if command == 'turn around':
-        mt.turn_right()
-        mt.turn_right()
-        return '''✔
+        if command == 'turn around':
+            mt.turn_right()
+            mt.turn_right()
+            return '''✔
 
 ---'''
-    if command == 'freemove':
-        cmd_selfmove()
-        return '''✔
+        if command == 'freemove':
+            cmd_selfmove()
+            return '''✔
 
 ---'''
                     
-    if command == 'heal':
-        play.heal()
+        if command == 'heal':
+            play.heal()
+                
+        if command == 'stats':
+            print('health:', play.health())
+            print('location: ({}, {})'.format(mt.get_x(), mt.get_y()))
 
-        
-    if command == 'stats':
-        print('health:', play.health())
-        print('location: ({}, {})'.format(mt.get_x(), mt.get_y()))
-
-    if command == 'inputs':
-        print(''' [go forward, turn right, turn left, turn around, location, freemove, heal, stats, input(current)]
+        if command == 'inputs':
+            print(''' [go forward, turn right, turn left, turn around, location, freemove, heal, stats, input(current)]
 ''')
-    else:
-        play.movement()
-        return 'you tripped and fell'
+        else:
+            play.movement()
+            return 'you tripped and fell'
 
-#------------------------------------------(Eh)--------------------------------------------------------------
+    #------------------------------------------(playgame)-------------------------------------------------------
+    def playgame(self):
+        play = Player()
+        while True: 
+            if play.mt.get_x() == 1 and play.mt.get_y() == 1:
+                play.take_damage(damage_taken)
+                play.stats(2)
 
-def cmd_selfmove():
-    play = Player()
-    play.selfmove(int(input("Steps => ")))
+            position = (play.mt.get_x(), play.mt.get_y())
+            if position in self.gu_mine:
+                print('you stepped on a gu mine! Oh no too bad youll be taking damage of 1 each step')
+                play.poison()
+                
+            if play.health() == 0:
+                print('''---
+    game over (ya suck)
+    ---''')
+                break
+
+            play.status()
+            
+            command = input("What dyou wanna do?\n => ")
+            result = self.process_command(play.mt, command)
+            print(result)
